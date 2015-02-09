7d56b8980202d079741d4ce83a349e933d3a5481
Hugo <Hugo@Hugo-PC>
2015-02-09 18:56:17 +0100

    contact

--- A2DF-Website/about.php
+++ A2DF-Website/about.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Nous connaître
-                    <small>Subheading</small>
+                    <small></small>
                 </h1>
                 <ol class="breadcrumb">
                     <li><a href="index.php">Accueil</a>
