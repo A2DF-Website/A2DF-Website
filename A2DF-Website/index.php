df5eb31cf41df865df14939cf480129de5e8c022
Alexandre <Alexandre@Altano-PC>
2015-02-09 17:16:49 +0100

    Index

--- A2DF-Website/index.php
+++ A2DF-Website/index.php
@@ -3,6 +3,10 @@
 ?>
 
 <body>
+
+    <!-- Navigation -->
+    
+
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
         <!-- Indicators -->
@@ -50,17 +54,22 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Welcome to Modern Business
+                    Bienvenue sur le site A2DF Informatique
                 </h1>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i> Bootstrap v3.2.0</h4>
+                        <h4><i class="fa fa-fw fa-clock-o"></i> Horaires d'ouvertures</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
+                        <p>
+                            <li>Lundi: 14h - 19h</li>
+                            <li>Du Mardi au Vendredi: 9h30 - 19h</li>
+                            <li>Samedi: 10h - 12h</li>
+                            <li>Fermé le dimanche</li>
+                        </p>
+                        <a href="contact.php" class="btn btn-default">Contact</a>
                     </div>
                 </div>
             </div>
