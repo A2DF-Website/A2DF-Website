eb4f2a41a1dc4e3a1bed6a106f94a685ad3d7628
Hugo <Hugo@Hugo-PC>
2015-02-12 18:50:31 +0100

    produit

--- A2DF-Website/produits.php
+++ A2DF-Website/produits.php
@@ -22,7 +22,7 @@ try {
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Produits
-                    <small>en magasin (selon stock disponible)</small>
+                    <small>- Disponibles en magasin (sous 72h selon stock)</small>
                 </h1>
                 <ol class="breadcrumb">
                     <li><a href="index.php">Accueil</a>
